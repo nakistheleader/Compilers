be16bc7baf3ddf59f82733ae76f66eb633e57e61
ap01lo <apolloone19@gmail.com>
2025-05-30 16:30:44 +0300

    Add files via upload

---
+++ Part_A3/simple-flex-code σοφιανιδης/token.h
@@ -0,0 +1,12 @@
+/* Το αρχείο token.h περιέχει τα tokens με τους αριθμούς που τους αντιστοιχούν, που χρησιμοποιούμε στον λεκτικό αναλυτή μας.
+   Εδώ έχουμε προσθέσει τα νέα tokens. */
+#define STORED_WORD 1
+#define DELIMITER 2
+#define INTCONST 3
+#define VARIABLE 4
+#define FACT_RULE_NAME 5
+#define COMMENT 6
+#define FLOAT 7
+#define STRING 8
+#define OPERATOR 9
+#define UNKNOWN_TOKEN 10
